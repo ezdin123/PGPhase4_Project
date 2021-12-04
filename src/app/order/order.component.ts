8741e144abe698fbf551b582fb0818e8157acde3
ezu <ezu.abas2010@yahoo.com>
2021-12-04 15:44:51 -0600

    adding order and services for messenger

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { MessengerService } from '../services/messenger.service';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  public ordersForm: FormGroup;
+  public products: any;
+  public orders: any;
+  public users: any;
+  public uname: String;
+  cartItems = [];
+  username:String;
+ // @Input() productItem: Products
+
+  constructor(private apiService: ApiService, private router: Router, private msg: MessengerService) { }
 
   ngOnInit(): void {
+    this.uname = sessionStorage.getItem("username");
+
+    // load get all products
+    this.getAllProducts();
+
   }
+  public getAllProducts() {
+    this.apiService.getAllProducts().subscribe(data => {
+      console.log(data);
+      this.products = data;
+    });
+  }
+
+  details(p_id) {
+    this.router.navigateByUrl('/products/details/' + p_id);
+  }
+
+  addtocart(p_id) {
+    console.log(this.uname);
+    console.log(p_id);
+    this.msg.sendMsg(this.products);
+   this.apiService.addOrders(p_id);
+  // this.router.navigateByUrl('/userorders/');
+
+  }
+
+   checkout() {
+     this.router.navigateByUrl('/checkout');
+   }
 
 }
